584174f0117fbb1c043ca9f4bb2f55158dff78b5
Riadh BENCHOUCHE <r.benchouche1@gmail.com>
2022-05-11 21:13:47 +0200

    Add Compact cards for doctors

---
+++ src/views/doctors/Doctors.vue
@@ -0,0 +1,108 @@
+<template>
+  <b-row>
+    <doctor-compact-card :doctors-data="data"></doctor-compact-card>
+  </b-row>
+</template>
+
+<script>
+import DoctorCompactCard from '@/views/doctors/doctorCompactCard.vue'
+
+export default {
+  components: {
+    DoctorCompactCard,
+  },
+  data() {
+    return {
+      data: [
+        {
+          id: 1,
+          fullName: 'Halsey Redmore',
+          company: 'CHU Mustapha bacha',
+          specialite: 'Chirurgie dentaire',
+          country: 'Albania',
+          contact: '(472) 607-9137',
+          email: 'hredmore1@imgur.com',
+          // eslint-disable-next-line global-require
+          avatar: require('@/assets/images/avatars/10.png'),
+        },
+        {
+          id: 2,
+          fullName: 'Marjory Sicely',
+          company: 'Oozz PVT LTD',
+          specialite: 'Chirurgie dentaire',
+          country: 'Russia',
+          contact: '(321) 264-4599',
+          email: 'msicely2@who.int',
+          // eslint-disable-next-line global-require
+          avatar: require('@/assets/images/avatars/1.png'),
+        },
+        {
+          id: 3,
+          fullName: 'Cyrill Risby',
+          company: 'Oozz PVT LTD',
+          specialite: 'Chirurgie dentaire',
+          country: 'China',
+          contact: '(923) 690-6806',
+          email: 'crisby3@wordpress.com',
+          // eslint-disable-next-line global-require
+          avatar: require('@/assets/images/avatars/9.png'),
+        },
+        {
+          id: 4,
+          fullName: 'Maggy Hurran',
+          company: 'Aimbo PVT LTD',
+          specialite: 'Chirurgie dentaire',
+          country: 'Pakistan',
+          contact: '(669) 914-1078',
+          email: 'mhurran4@yahoo.co.jp',
+          // eslint-disable-next-line global-require
+          avatar: require('@/assets/images/avatars/10.png'),
+        },
+        {
+          id: 5,
+          fullName: 'Cyrill Risby',
+          company: 'Oozz PVT LTD',
+          specialite: 'Chirurgie dentaire',
+          country: 'China',
+          contact: '(923) 690-6806',
+          email: 'crisby3@wordpress.com',
+          // eslint-disable-next-line global-require
+          avatar: require('@/assets/images/avatars/9.png'),
+        },
+        {
+          id: 6,
+          fullName: 'Maggy Hurran',
+          company: 'Aimbo PVT LTD',
+          specialite: 'Chirurgie dentaire',
+          country: 'Pakistan',
+          contact: '(669) 914-1078',
+          email: 'mhurran4@yahoo.co.jp',
+          // eslint-disable-next-line global-require
+          avatar: require('@/assets/images/avatars/10.png'),
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.per-page-selector {
+  width: 90px;
+}
+
+.v-select {
+  &.item-selector-title,
+  &.payment-selector {
+    background-color: #fff;
+
+    .dark-layout & {
+      background-color: unset;
+    }
+  }
+}
+</style>
+
+<style lang="scss">
+@import '~@core/scss/vue/libs/vue-select.scss';
+</style>
---
+++ src/views/doctors/doctorCompactCard.vue
@@ -0,0 +1,89 @@
+<template>
+  <b-row>
+    <b-col
+      v-for="(doctor) in doctorsData"
+      :key="doctor.id"
+      cols="4"
+    >
+      <b-card>
+        <div class="d-flex justify-content-start">
+          <b-avatar
+            :src="doctor.avatar"
+            :text="avatarText(doctor.fullName)"
+            size="104px"
+            rounded
+          />
+          <div class="d-flex flex-column ml-1">
+            <div class="mb-1">
+              <h4 class="mb-0 text-primary">
+                {{ doctor.fullName }}
+              </h4>
+              <table class="mt-2 mt-xl-0 w-100">
+                <tr>
+                  <th class="pb-50">
+                    <span class="font-weight-bold">Spécialité </span>
+                  </th>
+                  <td class="pb-50">
+                    {{ doctor.specialite }}
+                  </td>
+                </tr>
+                <tr>
+                  <th class="pb-50">
+                    <span class="font-weight-bold">Hopital</span>
+                  </th>
+                  <td class="pb-50 text-capitalize">
+                    {{ doctor.company }}
+                  </td>
+                </tr>
+                <tr>
+                  <th class="pb-50">
+                    <feather-icon
+                      icon="MapPinIcon"
+                      class="mr-50 "
+                    />
+                    <span class="font-weight-bold">Country</span>
+                  </th>
+                  <td class="pb-50 ml-1">
+                    {{ doctor.country }}
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </div>
+        </div>
+      </b-card>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+import {
+  BCard, BAvatar, BRow, BCol,
+} from 'bootstrap-vue'
+import { avatarText } from '@core/utils/filter'
+
+export default {
+  components: {
+    BCard,
+    BRow,
+    BCol,
+    BAvatar,
+  },
+  props: {
+    doctorsData: {
+      type: Array,
+      required: true,
+    },
+  },
+  setup(props) {
+    console.log(props.doctorsData)
+    return {
+      avatarText,
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
