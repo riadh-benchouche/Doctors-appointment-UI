2c20321b76c2feee81853543c56d7d3368f29d42
riadh-benchouche <r.benchouhce1@gmail.com>
2022-05-14 13:39:41 +0200

    Adding Doctor card with swiper

---
+++ src/views/doctors/DoctorCard.vue
@@ -0,0 +1,140 @@
+<template>
+  <swiper
+      class="swiper-multiple"
+      :options="swiperOptions"
+      :dir="$store.state.appConfig.isRTL ? 'rtl' : 'ltr'"
+  >
+    <swiper-slide
+        v-for="(doctor) in doctorsData"
+        :key="doctor.id"
+    >
+      <b-card
+          :img-src="require('@/assets/images/profile/user-uploads/timeline.jpg')"
+          img-alt="Profile Cover Photo"
+          img-top
+          class="card-profile"
+      >
+        <div class="profile-image-wrapper">
+          <div class="profile-image p-0">
+            <b-avatar
+                size="114"
+                variant="light"
+                :src="require('@/assets/images/profile/user-uploads/user-01.jpg')"
+            />
+          </div>
+        </div>
+        <h3>{{doctor.fullName}}</h3>
+        <h6 class="text-muted">
+          Spécialité: {{doctor.specialite}}
+        </h6>
+        <b-badge
+            class="profile-badge"
+            variant="light-primary"
+        >
+          {{doctor.company}}
+        </b-badge>
+        <p>
+          <feather-icon
+              icon="MapPinIcon"
+              class="mr-25"
+          />
+          {{doctor.country}}
+        </p>
+        <hr class="mb-2">
+
+        <!-- follower projects rank -->
+        <div class="d-flex justify-content-between align-items-center">
+          <div>
+            <h4 class="text-muted font-weight-bolder">
+              Rating
+            </h4>
+            <h1 class="mb-0">
+              4.3
+            </h1>
+            <star-rating
+                :star-size="20"
+                :show-rating="false"
+                :rounded-corners="true"
+            />
+          </div>
+          <div class="progress-wrapper w-50">
+            <b-row
+                v-for="bar in bars"
+                :key="bar.variant"
+            >
+              <b-col
+                  md="2"
+              >
+                {{ bar.variant }}
+              </b-col>
+              <b-col
+                  md="10"
+              >
+                <b-progress
+                    :key="bar.variant"
+                    :value="bar.value"
+                    class="mb-25"
+                />
+              </b-col>
+            </b-row>
+          </div>
+        </div>
+        <!--/ follower projects rank -->
+      </b-card>
+    </swiper-slide>
+
+    <div
+        slot="pagination"
+        class="swiper-pagination"
+    />
+  </swiper>
+</template>
+
+<script>
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+import StarRating from 'vue-star-rating'
+import {
+  BCard, BAvatar, BBadge, BProgress, BRow, BCol,
+} from 'bootstrap-vue'
+import 'swiper/css/swiper.css'
+
+export default {
+  components: {
+    BCard,
+    BBadge,
+    BAvatar,
+    BProgress,
+    Swiper,
+    SwiperSlide,
+    StarRating,
+    BRow,
+    BCol,
+  },
+  props: {
+    doctorsData: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      swiperOptions: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+      },
+      bars: [
+        { variant: '5', value: 75 },
+        { variant: '4', value: 75 },
+        { variant: '3', value: 75 },
+        { variant: '2', value: 75 },
+        { variant: '1', value: 75 },
+      ],
+    }
+  },
+
+}
+</script>
